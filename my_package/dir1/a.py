50848bd92f290bdc01eeb239d88e432c24fe961c
Alex Li <7alex7li@gmail.com>
2022-01-28 12:15:28 -0500

    from stackoverflow

---
+++ my_package/dir1/a.py
@@ -0,0 +1,2 @@
+from my_package.dir2 import b
+print("It was imported")
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+print("ah")
+setup(name='myproject', version='1.0', packages=find_packages())
